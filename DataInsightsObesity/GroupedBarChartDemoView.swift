6ac626626be2f7957b3331d0f535dc95813193ae
Harini <s.harini627@gmail.com>
2023-04-15 22:19:26 -0400

    push

--- DataInsightsObesity/GroupedBarChartDemoView.swift
+++ DataInsightsObesity/GroupedBarChartDemoView.swift
@@ -12,7 +12,6 @@ import SwiftUICharts
 struct GroupedBarChartDemoView: View {
     
     let data = makeData()
-    var har=3
     var body: some View {
             GroupedBarChart(chartData: data, groupSpacing: 25)
                 .touchOverlay(chartData: data, unit: .suffix(of: "patients"))
