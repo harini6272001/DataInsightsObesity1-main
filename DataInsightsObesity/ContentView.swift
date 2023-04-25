14de5ba1e4338c6301aa7af90e540739158da5f9
Harini <s.harini627@gmail.com>
2023-04-25 13:46:12 -0400

    SdOH Factor change

--- DataInsightsObesity/ContentView.swift
+++ DataInsightsObesity/ContentView.swift
@@ -14,7 +14,66 @@ struct ContentView: View {
             GroupedBarChartDemoView()
             PieChartDemoView()
             GroupedBarChartDemoView1()
+            SDOHMis(name: "Mississippi Top 10 Drivers of Comorbidity", headline: "Comorbidity: Type II Diabetes",
+                    top1:"Patient Age",
+                    top2:"% of Population that is Overweight",
+                    top3:"% of Population with Employer Sponsored Health Insurance",
+                    top4:"% Families with Single Female and Children in Poverty",
+                    top5:"Avgerage Amount Spent on All Household Expenses",
+                    top6:"% Population Black (Non-Hispanic)",
+                    top7:"% Asian People without Health Insurance",
+                    top8:"% of Medicare beneficiaries diagnosed with CKD",
+                    top9:"% Black People without Health Insurance",
+                    top10:"% Change in Rate of Stroke")
+            SDOHMis(name: "Mississippi Top 10 Drivers of Comorbidity", headline: "Comorbidity: Respiratory Disease",
+                    top1:"Patient Age",
+                    top2:"Low-Income Childhood Obesity Rate",
+                    top3:"Patient Gender",
+                    top4:"% of Medicare beneficiaries diagnosed with High Cholesterol",
+                    top5:"% of Jobs in Transport or Warehousing",
+                    top6:"% Asian Population without Health Insurance",
+                    top7:"% Change in Rate of Lower Respiratory Disease",
+                    top8:"Rate of Lower Respiratory Disease",
+                    top9:"% of Population that is Asian (non-Hispanic)",
+                    top10:"% of Change in Population with High School Diploma")
+            SDOHMis(name: "Mississippi Top 10 Drivers of Comorbidity", headline: "Comorbidity: Stroke",
+                    top1:"Patient Age",
+                    top2:"Rate of Stroke in Population",
+                    top3:"% of Population that is Hispanic",
+                    top4:"% of Medicare Beneficiaries Diagnosed with High Cholesterol",
+                    top5:"Rate of Coronary Heart Disease",
+                    top6:"% of Population that is Asian",
+                    top7:"% of Medicare Beneficiaries Diagnosed with Hypertension",
+                    top8:" % of Families in Poverty",
+                    top9:"% of Jobs in Administrative Support, Remediation Svcs",
+                    top10:"% of Population on Medicare")
+            SDOHMis(name: "Mississippi Top 10 Drivers of Comorbidity", headline: "Comorbidity: Hypertension",
+                    top1:"Patient Age",
+                    top2:"% of Adults self-reporting Depression",
+                    top3:"",
+                    top4:"% of Asian people without Health Insurance",
+                    top5:"Median Hispanic Household Income",
+                    top6:"Rate of Fitness and Recreation Centers",
+                    top7:"% of Medicare beneficiaries diagnosed with depression",
+                    top8:"% of Families in Poverty",
+                    top9:"Patient Zip",
+                    top10:"% of Adults self-reporting physician inactivity in past 30 days")
+            SDOHMis(name: "Mississippi Top 10 Drivers of Comorbidity", headline: "Comorbidity: Obesity-Related Cancer",
+                    top1:"Patient Age",
+                    top2:"Patient Gender",
+                    top3:"% of Jobs in Administrative Support, Remediation Svcs",
+                    top4:"% of Population with Health Insurance",
+                    top5:" % of Jobs in Health Care / Social Assistance",
+                    top6:"% of Population that is Asian",
+                    top7:"% of Jobs in Agriculture, Forestry, Fishing",
+                    top8:"% of Jobs in Mining, Oil, Gas, Quarryingy",
+                    top9:"Median Household Income",
+                    top10:"% of People with Public Health Insurance")
             
+          
+    
+
+
         }
         .tabViewStyle(.page(indexDisplayMode:.always))
     }
--- DataInsightsObesity/ContentViewTexas.swift
+++ DataInsightsObesity/ContentViewTexas.swift
@@ -14,6 +14,61 @@ struct ContentViewTexas: View {
             GroupedBarChartDemoViewTexas()
             GroupedBarChartDemoViewTexas1()
             PieChartDemoViewTexas()
+            SDOHTexas(name: "Texas Top 10 Drivers of Comorbidity", headline: "Comorbidity: Type II Diabetes",
+                    top1:"Patient Age",
+                    top2:"% of Population that is Overweight",
+                    top3:"% of Population with Employer Sponsored Health Insurance",
+                    top4:"% Families with Single Female and Children in Poverty",
+                    top5:"Avgerage Amount Spent on All Household Expenses",
+                    top6:"% Population Black (Non-Hispanic)",
+                    top7:"% Asian People without Health Insurance",
+                    top8:"% of Medicare beneficiaries diagnosed with CKD",
+                    top9:"% Black People without Health Insurance",
+                    top10:"% Change in Rate of Stroke")
+            SDOHTexas(name: "Texas Top 10 Drivers of Comorbidity", headline: "Comorbidity: Respiratory Disease",
+                    top1:"Patient Age",
+                    top2:"% of Population that is Overweight",
+                    top3:"% of Population with Employer Sponsored Health Insurance",
+                    top4:"% Families with Single Female and Children in Poverty",
+                    top5:"Avgerage Amount Spent on All Household Expenses",
+                    top6:"% Population Black (Non-Hispanic)",
+                    top7:"% Asian People without Health Insurance",
+                    top8:"% of Medicare beneficiaries diagnosed with CKD",
+                    top9:"% Black People without Health Insurance",
+                    top10:"% Change in Rate of Stroke")
+            SDOHTexas(name: "Texas Top 10 Drivers of Comorbidity", headline: "Comorbidity: Stroke",
+                    top1:"Patient Age",
+                    top2:"% of Population that is Overweight",
+                    top3:"% of Population with Employer Sponsored Health Insurance",
+                    top4:"% Families with Single Female and Children in Poverty",
+                    top5:"Avgerage Amount Spent on All Household Expenses",
+                    top6:"% Population Black (Non-Hispanic)",
+                    top7:"% Asian People without Health Insurance",
+                    top8:"% of Medicare beneficiaries diagnosed with CKD",
+                    top9:"% Black People without Health Insurance",
+                    top10:"% Change in Rate of Stroke")
+            SDOHTexas(name: "Texas Top 10 Drivers of Comorbidity", headline: "Comorbidity: Hypertension",
+                    top1:"Patient Age",
+                    top2:"% of Population that is Overweight",
+                    top3:"% of Population with Employer Sponsored Health Insurance",
+                    top4:"% Families with Single Female and Children in Poverty",
+                    top5:"Avgerage Amount Spent on All Household Expenses",
+                    top6:"% Population Black (Non-Hispanic)",
+                    top7:"% Asian People without Health Insurance",
+                    top8:"% of Medicare beneficiaries diagnosed with CKD",
+                    top9:"% Black People without Health Insurance",
+                    top10:"% Change in Rate of Stroke")
+            SDOHTexas(name: "Texas Top 10 Drivers of Comorbidity", headline: "Comorbidity: Obesity-Related Cancer",
+                    top1:"Patient Age",
+                    top2:"% of Population that is Overweight",
+                    top3:"% of Population with Employer Sponsored Health Insurance",
+                    top4:"% Families with Single Female and Children in Poverty",
+                    top5:"Avgerage Amount Spent on All Household Expenses",
+                    top6:"% Population Black (Non-Hispanic)",
+                    top7:"% Asian People without Health Insurance",
+                    top8:"% of Medicare beneficiaries diagnosed with CKD",
+                    top9:"% Black People without Health Insurance",
+                    top10:"% Change in Rate of Stroke")
             
         }
         .tabViewStyle(.page(indexDisplayMode:.always))
